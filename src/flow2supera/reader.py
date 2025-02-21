46ead9e4d6910d6ad4c28da3ab79d79b30e2a694
Yifan Chen <cyifan@sdfiana007.sdf.slac.stanford.edu>
2025-02-20 23:19:01 -0800

    attempt to remove larpix parser

--- src/flow2supera/reader.py
+++ src/flow2supera/reader.py
@@ -64,7 +64,8 @@ class Neutrino:
     
 class InputReader:
     
-    def __init__(self, parser_run_config, config=None):
+    #def __init__(self, parser_run_config, config=None):
+    def __init__(self, config=None):
 
         self._event_ids = None
         self._event_t0s = None
@@ -75,7 +76,7 @@ class InputReader:
         self._segments = None
         self._trajectories = None
         self._interactions = None
-        self._run_config = parser_run_config
+        #self._run_config = parser_run_config
         self._is_sim = False
         self._is_mpvmpr= False
         self._has_light=False
